01abe736391817af87562eef30dba5d62dbb17f3
thebeast0011 <abdulhaleem95@yahoo.com>
2021-09-17 18:04:08 +0800

    test push

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :friends, ecto_repos: [Friends.Repo]
 config :friends, Friends.Repo,
-  database: "friends_repo",
+  database: "friends_repo", #setting up database
   username: "postgres",
   password: "postgres",
   hostname: "localhost"
